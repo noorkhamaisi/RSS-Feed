bdbd4daf7f6a7c39989f9aa273989533a6e727b1
noorkhamaisi <khamaisenoor@gmail.com>
2021-10-03 15:34:54 +0300

    firstcommit

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 14:00:02 2021
+
+@author: get-a
+"""
+from flask import Flask
+
+import feedparser
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def showresults():
+    NewsFeed = feedparser.parse("http://www.ynet.co.il/Integration/StoryRss2.xml")
+    sumn=""
+    print ('Number of RSS posts :', len(NewsFeed.entries))
+    for i in range(1,len(NewsFeed.entries)):
+        entry = NewsFeed.entries[i]
+        print ('Post Title :',entry.title)
+        sumn=sumn+"/n"+entry.title
+        
+    return sumn
+        
+        
+        
+        
+        
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080)
